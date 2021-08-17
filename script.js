37d465707f61197c57d6a68f64b36c356e7ed8c5
Diogo <kanonmsgdiogo@outlook.pt>
2021-08-17 18:16:42 +0100

    Fixed hover functionality after button click. Added current grid value display

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
 const content = document.querySelector('.container');
+const numerion = document.querySelector('.numerino');
 
-buildGrid(16);
+let value = 16;
+let percentage = 100/value;
+buildgrid();
 
+let nbyf = document.createElement('h1');
+nbyf.textContent = value + " X " + value;
+numerion.appendChild(nbyf);
 
-
-function random_rgba() {
-    
+function random_rgba() {    
     var o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
 
-let allCellsOS = document.querySelectorAll('.cell');
-allCellsOS.forEach((cellOs) => {
-    cellOs.onmouseover = function (){
-        cellOs.setAttribute('style', `background-color: ${random_rgba()}`);
-    };
-})
-
-
-function changeGrid(){
-    let value = prompt("Choose a number, preferably less than 100");
-    while(value >= 100){
-        prompt("LMAO, I SAID less than 100");
-    }
-;
-
-    while(content.hasChildNodes()){
-        content.removeChild(content.lastChild);
-    }
-
-    buildGrid(value);
-}
-
-function buildGrid(value){
-    let percentage = 100/value;
+function buildgrid(){
     for(j = 0; j < value; j++){
         for(i = 0; i < value; i++){
-            let cell = document.createElement('div');
-            cell.classList.add('cell');
-            content.appendChild(cell);
-            cell.setAttribute('style', `width: ${percentage}%; height: ${percentage}%;`);
-
+            let cellzy = document.createElement('div');
+            cellzy.classList.add('cell');
+            content.appendChild(cellzy);
+            cellzy.style.width = `${percentage}%`;
+            cellzy.style.height = `${percentage}%`;
+            
+            cellzy.addEventListener('mouseover', () => {
+                cellzy.style.backgroundColor = `${random_rgba()}`;  
+            });
         }
-    }   
+    }
 }
 
-let buttino = document.querySelector('button');
-buttino.addEventListener("click", changeGrid);
 
+let buttino = document.querySelector('button');
+buttino.addEventListener("click", () => {
+    value = prompt("Choose a number, preferably less than 100");
+    while(value >= 60 || value == null || value < 0){
+        prompt("LMAO");
+    }
+
+    nbyf.textContent = value + " X " + value;
+    percentage = 100/value;
+    content.innerHTML = '';
+    buildgrid();
+});
 
 
 
